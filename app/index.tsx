cb37c61144f4f52d3e5d8cb064031bbae5d6e74f
williams bolu <williams.bolu99@gmail.com>
2026-07-29 15:39:44 +0100

    feat: integrate NativeWind with Tailwind CSS for styling and configure Metro bundler

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
